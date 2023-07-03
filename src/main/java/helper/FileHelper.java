76d157ef86a0644fa80ed13cc4ed5e33eb6c53fd
Bianca <bianca.burlacu1410@gmail.com>
2023-07-03 22:08:03 +0300

    Refactoring the file helper and step definition

--- src/main/java/helper/FileHelper.java
+++ src/main/java/helper/FileHelper.java
@@ -5,17 +5,8 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class FileHelper {
-    private final String filePath;
-
-    public FileHelper(String filePath) {
-        this.filePath = filePath;
-    }
-
-    public String getFilePath() {
-        return filePath;
-    }
-
-    public String readFromFile(String filePath) throws IOException{
-        return new String(Files.readAllBytes(Paths.get(filePath)));
+    public static String readFromFile(String fileName) throws IOException{
+        String path = "src/test/resources/requests/" + fileName;
+        return new String(Files.readAllBytes(Paths.get(path)));
     }
 }
--- src/test/java/tokenGenerationTests/StepDefinition.java
+++ src/test/java/tokenGenerationTests/StepDefinition.java
@@ -17,8 +17,6 @@ import static io.restassured.RestAssured.given;
 
 public class StepDefinition {
     private Response response;
-    private final FileHelper fileHelperValid = new FileHelper("src/test/resources/requests/validTokenGenerationRequest.json");
-    private final FileHelper fileHelperInvalid = new FileHelper("src/test/resources/requests/invalidTokenGenerationRequest.json");
 
     @Given("I have the base URL {string}")
     public void setBaseURL(String baseURL) {
@@ -27,7 +25,7 @@ public class StepDefinition {
 
     @When("I send a POST request to {string} with a valid request body")
     public void iSendAPOSTRequestToWithAValidRequestBody(String endpoint) throws IOException {
-        String requestBody = fileHelperValid.readFromFile(fileHelperValid.getFilePath());
+        String requestBody = FileHelper.readFromFile("validTokenGenerationRequest.json");
         response = given()
                 .contentType(ContentType.JSON)
                 .body(requestBody)
@@ -36,7 +34,7 @@ public class StepDefinition {
     }
     @When("I send a POST request to {string} with the an invalid request body:")
     public void iSendAPOSTRequestToWithTheAnInvalidRequestBody(String endpoint) throws IOException {
-        String requestBody = fileHelperInvalid.readFromFile(fileHelperInvalid.getFilePath());
+        String requestBody = FileHelper.readFromFile("invalidTokenGenerationRequest.json");
         response = given()
                 .contentType(ContentType.JSON)
                 .body(requestBody)
@@ -64,7 +62,6 @@ public class StepDefinition {
 
     @And("the response body should contain a reason field with the text {string}")
     public void theResponseBodyShouldContainAReasonFieldWithTheText(String expectedReason) {
-        System.out.println(response.then().log().body());
         response.then().body("reason",Matchers.equalTo(expectedReason));
     }
 
